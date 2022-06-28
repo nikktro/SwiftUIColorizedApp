1e7b2aa5901116ceb7971f89c8e4985bd13035a8
Nikolay Trofimov <nikktro@gmail.com>
2022-06-28 21:47:41 +0300

    Initial Commit

---
+++ ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Nikolay Trofimov on 28.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
